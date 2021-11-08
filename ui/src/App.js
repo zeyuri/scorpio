6ebab139da82cae61cd242dffe4e2b264e2fe0de
zeasy <jozeyuri@gmail.com>
2021-11-07 22:00:45 -0300

    feat(ui): change text for testing ci

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
@@ -15,11 +15,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn anything!
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
